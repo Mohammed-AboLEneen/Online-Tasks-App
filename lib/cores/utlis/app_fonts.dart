8552af667e57c9c154b9613d5555c76c7a9d787b
Mohammed-AboLEneen <mohmedhty1@gmail.com>
2024-06-04 05:20:30 +0300

    create project structure and class for fonts

---
+++ lib/cores/utlis/app_fonts.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+class AppFonts{
+
+  static TextStyle textStyle30Medium = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w500
+  );
+
+  static TextStyle textStyle15Regular = TextStyle(
+      fontSize: 15,
+      fontWeight: FontWeight.bold
+  );
+
+  static TextStyle textStyle13Medium = TextStyle(
+      fontSize: 13,
+      fontWeight: FontWeight.w500
+  );
+  static TextStyle textStyle12Regular = TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w400
+  );
+}
